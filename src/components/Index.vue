3d068f9457442fac83dc73df10ce9a8877d43ea4
Myles Shannon <mylios101@gmail.com>
2018-02-26 21:48:33 -0500

    Set/Get cookie

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -6,7 +6,6 @@
 
 <script>
 import Users from './Users.vue'
-import cookie from 'vue-cookie'
 
 export default {
   name: 'Index',
@@ -14,31 +13,28 @@ export default {
     Users
   },
   created: function () {
-    console.log('created', this);
-    if(cookie.get('unluckyOnes') === undefined) {
-      cookie.set('unluckyOnes', {users: [
-        {id: 1, username: 'Myles'}
-      ]});
+    // console.log('created', this);
+    if(!this.$cookie.get('users')) {
+      this.$cookie.set('users', JSON.stringify([]));
     }
     this.$data.getUsers();
   },
   data: function() {
+    let data = this;
     return {
-      users: [
-        {id: 0, username: 'Myles'},
-        {id: 1, username: 'Stephen'}
-      ],
+      users: [],
       getUsers: function() {
         // get list off users from cookie
-        this.users = cookie.get('unluckyOnes');
-        return this.users;
+        let users = JSON.parse(data.$cookie.get('users'));
+        this.users = users;
+        return users;
       },
       addUser: function(username) {
         // add a new user, return list of new users
         let existingUsers = this.users;
         let newUserId = existingUsers[existingUsers.length - 1].id + 1;
         let newUserList = existingUsers.push({id: newUserId, username: username});
-        cookie.set('unluckyOnes', newUserList);
+        data.$cookie.set('users', JSON.parse(newUserList));
         return newUserList;
       },
       removeUser: function(userId) {
@@ -47,7 +43,7 @@ export default {
         existingUsers.forEach(function(user, index) {
           if(user.id === userId) {
             let newUserList = existingUsers.splice(index, 1);
-            cookie.set('unluckyOnes', newUserList);
+            data.$cookie.set('users', JSON.parse(newUserList));
             return newUserList;
           }
         })
